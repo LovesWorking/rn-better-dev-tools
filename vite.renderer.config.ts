3581a6b4c69f6332751310b462810e9db3280ded
lovesworking <austinlovesworking@gmail.com>
2025-03-31 09:13:33 -0600

    Fix build process by adding custom TanStack package handling

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,7 +1,35 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { builtinModules } from "module";
+import * as path from "path";
 
 // https://vitejs.dev/config
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [
+        "@tanstack/query-devtools",
+        "@tanstack/react-query-devtools",
+        "@tanstack/react-query",
+        ...builtinModules,
+      ],
+    },
+  },
+  resolve: {
+    alias: {
+      "@tanstack/react-query-devtools": path.resolve(
+        __dirname,
+        "node_modules/@tanstack/react-query-devtools"
+      ),
+      "@tanstack/query-devtools": path.resolve(
+        __dirname,
+        "node_modules/@tanstack/query-devtools"
+      ),
+      "@tanstack/react-query": path.resolve(
+        __dirname,
+        "node_modules/@tanstack/react-query"
+      ),
+    },
+  },
 });
